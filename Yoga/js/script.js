6a80a115d48d988e0294271c4a88888eedbaf32f
Aleksandr Serov <sashaserov2010@gmail.com>
2018-10-25 22:45:42 +0300

    timer in one function

--- Yoga/js/script.js
+++ Yoga/js/script.js
@@ -28,51 +28,53 @@ window.addEventListener("DOMContentLoaded", () => {
 			showTabContent(tab.indexOf(target));
 		}
 	});
+	
 	// Timer 
-	let deadline = "2018-12-18";
-	let getTimeRemaining = (endtime) => {
-		let t = Date.parse(endtime) - Date.parse(new Date()),
-			seconds = Math.floor((t / 1000) % 60),
-			minutes = Math.floor((t / 1000 / 60) % 60),
-			hours = Math.floor((t / (1000 * 60 * 60)));
-
-		if (seconds < 10) {
-			seconds = "0" + seconds;
-		}
-		if (minutes < 10) {
-			minutes = "0" + minutes;
-		}
-		if (hours < 10) {
-			hours = "0" + hours;
-		}
-		return {
-			"total": t,
-			"hours": hours,
-			"minutes": minutes,
-			"seconds": seconds
-		};
-	};
-
-	let setClock = (id, endtime) => {
-		let timer = document.getElementById(id),
-			hours = timer.querySelector(".hours"),
-			minutes = timer.querySelector(".minutes"),
-			seconds = timer.querySelector(".seconds");
-		let updateClock = () => {
-			let t = getTimeRemaining(endtime);
-			if (t.total <= 0) {
-				clearInterval(timeInterval);
-				hours.textContent = "00";
-				minutes.textContent = "00";
-				seconds.textContent = "00";
-			} else {
-				hours.textContent = t.hours;
-				minutes.textContent = t.minutes;
-				seconds.textContent = t.seconds;
+	let setTimer = (deadline, id) => {
+		let getTimeRemaining = (deadline) => {
+			let t = Date.parse(deadline) - Date.parse(new Date()),
+				seconds = Math.floor((t / 1000) % 60),
+				minutes = Math.floor((t / 1000 / 60) % 60),
+				hours = Math.floor((t / (1000 * 60 * 60)));
+	
+			if (seconds < 10) {
+				seconds = "0" + seconds;
 			}
+			if (minutes < 10) {
+				minutes = "0" + minutes;
+			}
+			if (hours < 10) {
+				hours = "0" + hours;
+			}
+			return {
+				"total": t,
+				"hours": hours,
+				"minutes": minutes,
+				"seconds": seconds
+			};
 		};
-		let timeInterval = setInterval(updateClock, 1000);
+		let setClock = (id, deadline) => {
+			let timer = document.getElementById(id),
+				hours = timer.querySelector(".hours"),
+				minutes = timer.querySelector(".minutes"),
+				seconds = timer.querySelector(".seconds");
+			let updateClock = () => {
+				let t = getTimeRemaining(deadline);
+				if (t.total <= 0) {
+					clearInterval(timeInterval);
+					hours.textContent = "00";
+					minutes.textContent = "00";
+					seconds.textContent = "00";
+				} else {
+					hours.textContent = t.hours;
+					minutes.textContent = t.minutes;
+					seconds.textContent = t.seconds;
+				}
+			};
+			let timeInterval = setInterval(updateClock, 1000);
+		};
+		setClock(id, deadline);
 	};
-	setClock("timer", deadline);
 
+	setTimer('2018-12-18', 'timer');
 });
